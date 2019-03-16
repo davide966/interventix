ce8282e4d91cbcd72a8da8c5c654d8a348a37ce3
davide966 <dabuck@libero.it>
2019-03-16 15:30:38 +0100

    “message”

---
+++ app/pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <f7-page>
+    <f7-navbar :title="$root.config.title" />
+    <f7-block>
+      <f7-button big fill raised bg="blue" href="http://buccoliero.com" external>Interventi</f7-button>
+    </f7-block>
+    <f7-block>
+      <f7-button big fill raised bg="green" href="http://buccoliero.com" external>Clienti</f7-button>
+    </f7-block>
+    <f7-block inner inset>{{$lang('text')}}</f7-block>
+  </f7-page>
+</template>
